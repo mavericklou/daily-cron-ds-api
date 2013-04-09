63ea1dda9a093155b920e16851f765a6b2940ece
maverick <maverick@factual.com>
2013-04-09 17:28:23 +0800

    use /Users/maverick to make it OS independent

--- request_stat/request-stat.sh
+++ request_stat/request-stat.sh
@@ -4,7 +4,7 @@ MONTH=`date +%Y-%m --date="$1"`
 INPUT=/apps/ds-api/log/$MONTH/*.$DAY.log
 OUTPUT=/apps/ds-api/request-report/$MONTH/$DAY
 
-cd /home/maverick/bin
+cd $HOME/bin
 ./request-hadoop.sh $INPUT $OUTPUT
 
 hadoop fs -cat $OUTPUT/part* | sort | mail -s "ds-api-prod: Daily request statistic $DAY" maverick@factual.com,ds-api@factual.com
--- trace/filter-trace.sh
+++ trace/filter-trace.sh
@@ -4,5 +4,5 @@ MONTH=`date +%Y-%m --date="$1"`
 INPUT=/apps/ds-api/log/$MONTH/*.$YESTERDAY.log
 OUTPUT=/apps/ds-api/trace/$MONTH/$YESTERDAY
 
-cd /home/maverick/bin
+cd $HOME/bin
 ./trace-hadoop.sh $INPUT $OUTPUT
