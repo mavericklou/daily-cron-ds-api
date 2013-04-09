8dc19fca026f44033fabf3900166fd21b4a2006e
maverick <maverick@factual.com>
2013-04-10 00:10:17 +0800

    add KeyFieldBasedComparator for sorting request report

--- request_stat/request-hadoop.sh
+++ request_stat/request-hadoop.sh
@@ -2,6 +2,9 @@ echo "INPUT: $1"
 echo "OUTPUT: $2"
 hadoop jar /etc/alternatives/hadoop-lib/contrib/streaming/hadoop-streaming-0.20.2-cdh3u4.jar \
 -D mapred.output.compress=false \
+-D mapred.output.key.comparator.class=org.apache.hadoop.mapred.lib.KeyFieldBasedComparator \
+-D map.output.key.field.separator=' ' \
+-D mapred.text.key.comparator.options='-k1,1r -k3,3' \
 -input $1 \
 -output $2 \
 -file request-mapper.rb \
