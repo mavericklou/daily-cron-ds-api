d22b66e266f99e32642b8bd9d5c11e25d39af39f
maverick <maverick@factual.com>
2013-04-10 00:12:18 -0700

    do not use deprecated --jobconf

--- trace/trace-hadoop.sh
+++ trace/trace-hadoop.sh
@@ -1,8 +1,8 @@
 echo "INPUT: $1"
 echo "OUTPUT: $2"
 hadoop jar /etc/alternatives/hadoop-lib/contrib/streaming/hadoop-streaming-0.20.2-cdh3u4.jar \
+-D mapred.output.compress=false \
 -input $1 \
 -output $2 \
 -file trace.rb \
--mapper 'ruby trace.rb' \
--jobconf mapred.output.compress=false
+-mapper 'ruby trace.rb'
