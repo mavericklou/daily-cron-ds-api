c13dcb2c3e9b34b6f356c25cb1d6247f269a4377
maverick <maverick@factual.com>
2013-07-22 23:57:53 -0700

    gz log per directory

---
+++ zdTrace.sh
@@ -0,0 +1,6 @@
+FILES=`hadoop fs -ls $1/part-????? | grep -o "/apps.*$"`
+
+for file in $FILES; do
+  echo "##################  compressing $file  ####################"
+  hadoop fs -text $file | gzip -c | hadoop fs -put - $file.gz
+done
